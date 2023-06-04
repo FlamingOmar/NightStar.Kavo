51288a6ec6fff74896494d453c48177e1a759ffd
FireCat <134200126+FlamingOmar@users.noreply.github.com>
2023-06-04 20:37:53 +0300

    Update KavoUi.lua

--- KavoUi.lua
+++ KavoUi.lua
@@ -117,11 +117,11 @@ local themeStyles = {
 
         Background = Color3.fromRGB(0, 0, 0),
 
-        Header = Color3.fromRGB(0, 0, 0),
+        Header = Color3.fromRGB(15, 15, 15),
 
         TextColor = Color3.fromRGB(255,255,255),
 
-        ElementColor = Color3.fromRGB(0, 0, 0)
+        ElementColor = Color3.fromRGB(15, 15, 15)
     }
 }
 local oldTheme = ""
