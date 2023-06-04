586d1ba8def727958c767e3eb700fe3d828fd373
FlamingOmar <134200126+FlamingOmar@users.noreply.github.com>
2023-06-04 14:37:37 +0300

    Update KavoUi.lua

--- KavoUi.lua
+++ KavoUi.lua
@@ -117,11 +117,11 @@ local themeStyles = {
 
         Background = Color3.fromRGB(0, 0, 0),
 
-        Header = Color3.fromRGB(10, 10, 10),
+        Header = Color3.fromRGB(0, 0, 0),
 
         TextColor = Color3.fromRGB(255,255,255),
 
-        ElementColor = Color3.fromRGB(10, 10, 10)
+        ElementColor = Color3.fromRGB(0, 0, 0)
     }
 }
 local oldTheme = ""
